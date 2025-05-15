873540ebf7b2ab455266c150e95066d69f369686
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2025-05-15 13:32:15 +0530

    update

--- render-build.sh
+++ render-build.sh
@@ -4,4 +4,5 @@ echo "⬇️ Downloading Chromium snapshot from Google..."
 mkdir -p ./chromium
 curl -o chromium/chrome.zip https://storage.googleapis.com/chromium-browser-snapshots/Linux_x64/1108766/chrome-linux.zip
 unzip -q chromium/chrome.zip -d chromium/
+chmod +x chromium/chrome-linux/chrome
 echo "✅ Chromium extracted!"
