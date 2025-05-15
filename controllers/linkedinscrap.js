f2174eb7f41be9e42d15e0ae6fc723a269838b80
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2025-05-15 13:27:27 +0530

    update

--- controllers/linkedinscrap.js
+++ controllers/linkedinscrap.js
@@ -35,7 +35,7 @@ export const linkedinscrap = async (req, res) => {
   try {
     console.log(`[SCRAPER] Launching Puppeteer browser...`);
     browser = await puppeteer.launch({
-     executablePath: '/usr/bin/chromium-browser',
+      executablePath: path.resolve('./chromium/chrome-linux/chrome'),
       headless:true,
     
        args: ['--no-sandbox', '--disable-setuid-sandbox'],
--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-echo "➡️ Installing Chromium..."
-apt-get update
-apt-get install -y chromium-browser
-echo "✅ Chromium installed!"
+echo "⬇️ Downloading Chromium snapshot from Google..."
+mkdir -p ./chromium
+curl -o chromium/chrome.zip https://storage.googleapis.com/chromium-browser-snapshots/Linux_x64/1108766/chrome-linux.zip
+unzip -q chromium/chrome.zip -d chromium/
+echo "✅ Chromium extracted!"
